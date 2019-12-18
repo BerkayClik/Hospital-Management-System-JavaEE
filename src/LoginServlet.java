bf2347d72667dc5827a59e46bea7ddd2daa2ece6
dogukan1106 <56073555+dogukan1106@users.noreply.github.com>
2019-12-18 16:49:36 +0300

    LoginAuthenticationFixed
    :+1:

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
+import java.sql.ResultSet;
 
 public class LoginServlet extends HttpServlet {
     DB_Handler handler;
@@ -11,10 +13,36 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        password = new Hash(password,email).hash();
 
         //handler.init();
-
-        if(email.equals("dogukan.duduoglu@ozu.edu.tr")){//todo verify user
+        int role = 0;
+        try {
+          // PreparedStatement pstmt = handler.getConn().prepareStatement("select role_id from user where email = ? and pw = ?");
+             handler = new DB_Handler();
+             handler.init();
+           PreparedStatement pstmt = handler.getConn().prepareStatement("select role_id from users where email = ? and pw = ?");
+           pstmt.setString(1,email);
+           pstmt.setString(2,password);
+           ResultSet rs = pstmt.executeQuery();
+           while(rs.next()){
+               role = rs.getInt(1);
+           }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        if(role != 0){
+            response.addCookie(new Cookie("role_id",""+role));
+            response.addCookie(new Cookie("email",email));
+            response.sendRedirect("/user");
+            System.out.println(role);
+        }
+        else{
+            response.sendRedirect("index.jsp");
+            System.out.println(role);
+        }
+      /*  if(email.equals("dogukan.duduoglu@ozu.edu.tr")){//todo verify user
             response.addCookie(new Cookie("JX_userstuff",email));
             response.sendRedirect("/user");
 
@@ -22,7 +50,7 @@ public class LoginServlet extends HttpServlet {
             request.setAttribute("status","fail");
             request.setAttribute("email", email);
             request.getRequestDispatcher("index.jsp").forward(request, response);
-        }
+        }*/
 
 
     }
